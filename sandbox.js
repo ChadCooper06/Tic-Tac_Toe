ee0cd93a25fbec66600db3c6c232589c76d7220c
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-08 20:04:06 +0000

    added pseudo and some sandbox

--- sandbox.js
+++ sandbox.js
@@ -3,9 +3,11 @@
 //Constants
 
 const INIT = ""//run a function on initialization- same function that will reset it
-const WIN_CONS = "[[0,1,2],[,3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]";
+const WIN_CONS = [[0,1,2],[,3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 const TITLE = document.getElementById("title").innerHTML("Tic-Tac-Toe");
 const RESET = document.getElementById("resetBtn").innerHTML("Reset");
+const PLAYER1 = "X";
+const PLAYER2 = "O";
 
 //Classes
 
@@ -19,8 +21,14 @@ class App {
 
     }
 
+    async checkForWin() {
+        if (currentClicks > 5) {
+            //clickhandler
+        }
+    }
+
     currentState() {
-        //board and squares are in here as wel as title and reset
+        //board and squares are in here as well as title and reset
     }
 
     currentPlayer(){
@@ -37,11 +45,36 @@ class App {
 
 //VIEW
 
+class GameView {
+    constructor() {
+
+    }
+}
+
+render() {
+
+}
+
 currentState() {
-    
+
+
 }
 
 
 
 //CONTROLLER
+
+class GameController {
+    constructor(App, GameView) {
+        this.a = App;
+        this.g = GameView;
+    }
+}
+
+clickHandler() {
+    currentClicks = clicks;
+    for(let i=0; i>5; i++) {
+
+    }
+}
 
