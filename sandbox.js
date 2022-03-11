83fa4e5614cad94bdd5cd5a4443c2479ede92aad
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-11 00:56:07 +0000

    places at least one letter on board before a win

--- sandbox.js
+++ sandbox.js
@@ -1,33 +1,37 @@
-const container = document.getElementById("gameBoard");
-const squareArray = [];
-const currentPlayer = "X";
+    //only one class 
+let container = document.getElementById("gameBoard");
+let squareArray = [];
+let currentPlayer = "X";
 
 //CONTROLLER
 
-function gameOver(alert){
-    
-    document.getElementById("alert").innerHTML = alert;
-    alert(this.state +" wins!");
-    
-    
-/*if(winGame !== false){
-    } else {
-        alert("I see only losers")
-    }*/
+    //runs when game is over
+function gameOver(message){
+    document.getElementById("winner").innerHTML = message;
+    container.style.display = "none";
+    document.getElementById("gameOver").style.display = "block";
 }
 
     //runs if the game is a draw
 function drawGame(){
     let shouldReturn = true;
     squareArray.forEach(({state}) => {
-        if(state == "") shouldReturn = false;
+        if(state === "") shouldReturn = false;//game is over if true so if any aren't true this is false
     });
     return shouldReturn;
 }
 
+
 function winGame(){
     const winCons = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 6], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 6], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6],
     ];
     for(let i=0; i<winCons.length; i++) {
         const [a, b, c] = winCons[i];
@@ -52,17 +56,14 @@ class GameSquare{
     }
     clicked() {
         this.state = currentPlayer;
-        
         this.element.classList.remove("notClicked");
         this.element.onclick = function() {
             return false;
         };
-        this.element.querySelector("#p").innerHTML = this.state;
-        
+        this.element.querySelector("p").innerHTML = this.state;
            if(winGame()) return gameOver(this.state + " wins!")
-           if(drawGame()) return gameOver("No winner");
-        this.currentPlayer == "X" ? (currentPlayer = "O") : (currentPlayer = "X");
-    
+           if(drawGame()) return gameOver("I see only losers");
+        currentPlayer == "X" ? (currentPlayer = "O") : (currentPlayer = "X");
     }
 }
 
@@ -70,13 +71,10 @@ for(let index = 0; index < 9; index++) {
     const div = document.createElement("div");
     div.classList.add("square", "notClicked");
     const square = new GameSquare(div, index);
-    square.onclick = () => {
+    div.onclick = () => {
         square.clicked();
     };
     div.appendChild(document.createElement("p"));
     container.appendChild(div);
     squareArray.push(square);    
 }
-
-   //function addEventListener('click', )
-
